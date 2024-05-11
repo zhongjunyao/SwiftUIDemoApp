56da4575c99ac1c38b9f63f5938fc7a19182396e
Robin.J.Y.ZHONG <15914468806@139.com>
2024-05-11 11:28:53 +0800

    Initial Commit

---
+++ SwiftUIDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIDemoApp
+//
+//  Created by ROBIN.J.Y.ZHONG on 2024/5/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIDemoApp/SwiftUIDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoAppApp.swift
+//  SwiftUIDemoApp
+//
+//  Created by ROBIN.J.Y.ZHONG on 2024/5/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
